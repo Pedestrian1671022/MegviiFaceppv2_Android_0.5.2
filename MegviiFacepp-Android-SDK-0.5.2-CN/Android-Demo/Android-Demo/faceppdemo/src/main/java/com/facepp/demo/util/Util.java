9cac1881e15962aab76395fb5021a0cb67486884
Pedestrian1671022 <1172012891@qq.com>
2018-03-15 16:39:45 +0800

    first commit

---
+++ MegviiFacepp-Android-SDK-0.5.2-CN/Android-Demo/Android-Demo/faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -0,0 +1,11 @@
+package com.facepp.demo.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "phoB_4fKHs2kQwK0Gi7rk3S7IDHG2wrh";
+	public static String API_SECRET = "yJ6F00DnYiaBQFc7zxXbkgqUyQKl9rKK";
+	
+	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v2/auth";
+	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v2/auth";
+}
---
+++ MegviiFacepp-Android-SDK-0.5.2-CN/README.txt
@@ -0,0 +1,8 @@
+Face++ 人脸 SDK（Android）
+===
+
+1. `Facepp_Android/` 目录下存放的是 Face++ 人脸 SDK 的 C 接口 SDK；
+2. 如果您需要了解该 SDK 的接口及相关信息请移步至 `doc/` 目录下；
+3. 如果您想要快速集成，或者学习使用该 SDK 做一个简单的 Demo，请移步至 https://github.com/FacePlusPlus/MegviiFacepp-Android-SDK。
+
+
